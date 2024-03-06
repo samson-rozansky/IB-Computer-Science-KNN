3915bdb25adaf4892d02d7577591c869139e8197
Brian Zhang <70183960+brianzjk@users.noreply.github.com>
2024-03-06 13:05:24 -0500

    change delimiter for data raw to comma

--- scripts/boxplot_raw.py
+++ scripts/boxplot_raw.py
@@ -6,7 +6,7 @@ import pandas as pd
 ROOT = pathlib.Path(__file__).parent.parent.resolve().joinpath("data")
 OUTPUT = pathlib.Path(__file__).parent.parent.resolve().joinpath("figs")
 
-data = pd.read_csv(ROOT.joinpath("data_raw.csv"), delimiter = ';')
+data = pd.read_csv(ROOT.joinpath("data_raw.csv"))
 
 fig = plt.figure()
 ax = fig.add_axes([0, 0, 1, 1])
