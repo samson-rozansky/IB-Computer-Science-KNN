b9011a3dcf53081863cd7f539763090954e7528f
Brian Zhang <70183960+brianzjk@users.noreply.github.com>
2024-03-06 14:03:30 -0500

    tsne

---
+++ scripts/tsne.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+from matplotlib import colormaps
+import pathlib
+import pandas as pd
+import plotly.express as px
+
+from sklearn import metrics, manifold
+from sklearn.metrics import precision_score, accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.manifold import TSNE
+
+ROOT = pathlib.Path(__file__).parent.parent.resolve().joinpath("data")
+OUTPUT = pathlib.Path(__file__).parent.parent.resolve().joinpath("figs")
+
+DATA_FILE = ROOT.joinpath("data_normal.csv")
+
+if not DATA_FILE.is_file():
+    import preprocess
+
+data = pd.read_csv(DATA_FILE)
+
+# Create feature and target arrays
+X = data.drop(columns = ['Target'])
+y = data['Target']
+
+tsne = TSNE(n_components=2, random_state=42, init='pca')
+X_tsne = tsne.fit_transform(X)
+
+x_0 = []
+y_0 = []
+x_1 = []
+y_1 = []
+
+for i in range(y.size):
+    if (y[i] == 0):
+        x_0.append(X_tsne[i, 0])
+        y_0.append(X_tsne[i, 1])
+    else:
+        x_1.append(X_tsne[i, 0])
+        y_1.append(X_tsne[i, 1])
+
+plt.scatter(x=x_0, y=y_0, c='red', label='Dropout')
+plt.scatter(x=x_1, y=y_1, c='blue', label='Graduate')
+plt.ylabel('Second t-SNE')
+plt.xlabel('First t-SNE')
+plt.legend()
+
+plt.show()
+
+
